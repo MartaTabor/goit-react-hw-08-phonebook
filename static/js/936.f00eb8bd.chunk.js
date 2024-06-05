1528c780c76cfe81b0bb25dc719f04e56dc9d7ea
MartaTabor <139591341+MartaTabor@users.noreply.github.com>
2024-06-05 21:55:37 +0000

    Deploying to gh-pages from @ MartaTabor/goit-react-hw-08-phonebook@86be449a34e3dfbb91ea7d1be1cd699c828bdb43 🚀

--- static/js/936.f00eb8bd.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{4936:function(e,n,o){o.r(n),o.d(n,{default:function(){return l}});var r="Home_container__w+u63",t="Home_one__5Vblw",s=o(184),l=function(){return(0,s.jsxs)("div",{className:r,children:[(0,s.jsx)("h1",{children:"Welcome to The Phonebook"}),(0,s.jsx)("p",{className:t,children:"The best user firendly online based phonebook"}),(0,s.jsx)("p",{children:"Please register or log in to start"})]})}}}]);
-//# sourceMappingURL=936.f00eb8bd.chunk.js.map
---
+++ static/js/936.ff477ae4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{4936:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var r="Home_container__w+u63",t="Home_one__5Vblw",s=o(184),c=function(){return(0,s.jsxs)("div",{className:r,children:[(0,s.jsx)("h1",{children:"Welcome to The Phonebook"}),(0,s.jsx)("p",{className:t,children:"The best user firendly online based phonebook"})]})}}}]);
+//# sourceMappingURL=936.ff477ae4.chunk.js.map
